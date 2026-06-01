790bd0a9a7f8abf53e727f3ea6c70b04a213f947
Ahmed Jrainija <ahmedjrainija@gmail.com>
2026-06-01 11:27:56 +0100

    update Cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://mern-e-commerce-henna-seven.vercel.app"],
   credentials: true
 }));
 
